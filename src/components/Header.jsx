1d953c6452676263fc4c3a43f095f3e4bbdc421f
Tinku4007 <tinkusaini1252@gmail.com>
2024-03-11 13:19:30 +0530

    minur issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import { Box, Container, Stack, Typography } from '@mui/material'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
-
+import Drawer from '@mui/material/Drawer';
+import { GiHamburgerMenu } from "react-icons/gi";
 const data =
     [
         {
@@ -23,34 +23,66 @@ const data =
     ]
 
 const Header = () => {
+    const [openDrawer, setOpenDrawer] = React.useState(false)
     return (
-        <header className='bg-pink-50 p-2'>
-            <Container>
-                <Stack flexDirection='row' justifyContent='space-between' >
-                    <Box>
-                        <Typography variant='h5' fontSize='18px' fontWeight={600}>
-                            Tinku SaiNi
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <ul className='flex items-center gap-10'>
-                            {data?.map((item, i) => (
-                                <li key={i}>
-                                    <HashLink to={item.to}>
-                                        <span>{item.name}</span>
-                                    </HashLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </Box>
-                    <Box>
-                        <Typography variant='body1' className='text-sm'>
-                            Email
-                        </Typography>
-                    </Box>
-                </Stack>
-            </Container>
-        </header>
+        <>
+            <header className='p-2 '>
+                <Container>
+                    <Stack flexDirection='row' justifyContent='space-between' >
+                        <Box>
+                            <Typography variant='h5' fontSize='18px' fontWeight={600}>
+                                Tinku SaiNi
+                            </Typography>
+                        </Box>
+                        <Box className='header'>
+                            <ul className='flex items-center gap-10'>
+                                {data?.map((item, i) => (
+                                    <li key={i}>
+                                        <HashLink to={item.to}>
+                                            <span>{item.name}</span>
+                                        </HashLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Box>
+                        <Box className="toggle">
+                            <Typography  onClick={() => setOpenDrawer(true)} variant='body1' className='text-sm'>
+                                <GiHamburgerMenu/>
+                            </Typography>
+                        </Box>
+                    </Stack>
+                </Container>
+            </header>
+            <Drawer open={openDrawer} onClose={()=>setOpenDrawer(false)} className=' header-mobile'>
+            <header className='p-2 w-[50vw]'>
+                <Container>
+                    <Stack justifyContent='space-between' gap="10px" >
+                        <Box>
+                            <Typography variant='h5' fontSize='18px' fontWeight={600}>
+                                Tinku SaiNi
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <ul className='flex flex-col gap-3'>
+                                {data?.map((item, i) => (
+                                    <li key={i}>
+                                        <HashLink to={item.to}>
+                                            <span>{item.name}</span>
+                                        </HashLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Box>
+                        <Box>
+                            <Typography onClick={() => setOpenDrawer(true)} variant='body1' className='text-sm'>
+                                Email
+                            </Typography>
+                        </Box>
+                    </Stack>
+                </Container>
+            </header>
+            </Drawer>
+        </>
     )
 }
 
